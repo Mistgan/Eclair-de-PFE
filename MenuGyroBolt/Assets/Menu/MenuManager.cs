afa1f1fff1403c57effeff5d16e33d3242eb3341
GaetanFaucherEfrei <gaetan.faucher@efrei.net>
2020-01-15 23:07:46 +0100

    Ajout menu
    ajout menu

---
+++ MenuGyroBolt/Assets/Menu/MenuManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuManager : MonoBehaviour {
+
+	public GameObject mainMenu, weaponsMenu, background;
+
+	private Animator mainMenuAnim,weaponsMenuAnim,backgroundAnim;
+	private bool isMainMenuOnLeft;
+
+	// Use this for initialization
+	void Start () {
+		mainMenuAnim = mainMenu.GetComponent<Animator>();
+		weaponsMenuAnim = weaponsMenu.GetComponent<Animator>();
+		backgroundAnim = background.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// fonction des boutons du menu
+
+	public void OnWeaponsButtonClick(){
+		print("OnWeaponsButtonClick");
+		if(isMainMenuOnLeft==false){
+			mainMenuAnim.SetBool("isMovingLeft",true);
+			weaponsMenuAnim.SetBool("isMovingIn",true);
+			backgroundAnim.SetBool("isTurning",true);
+			isMainMenuOnLeft =true;
+		}else{
+			mainMenuAnim.SetBool("isMovingLeft",false);
+			weaponsMenuAnim.SetBool("isMovingIn",false);
+			backgroundAnim.SetBool("isTurning",false);
+			isMainMenuOnLeft =false;
+		}
+	}
+}
